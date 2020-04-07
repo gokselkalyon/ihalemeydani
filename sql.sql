8f8325169045d5e04f5d9a2cc60e79f71fdc4be1
salıh encevık <salihencevik@gmail.com>
2020-04-07 21:23:55 +0300

    role ve kullanıcı tablolarının sql kodları

--- sql.sql
+++ sql.sql
@@ -1,4 +1,25 @@
-///////// salih ençevik ////////
+----///////// salih ençevik //////// 
+create table Claim
+(
+	Id int identity primary key,
+	Text nvarchar(50)
+)
+go
+create table Role
+(
+	Id int identity primary key,
+	Name nvarchar(50)
+)
+go
+create table RoleClaim
+(
+	Id int identity primary key,
+	RoleId int,
+	ClaimId int
+	Constraint FK_RoleClaimId_RoleId foreign key (RoleId) references Role(Id),
+	Constraint FK_RoleClaimId_ClaimId foreign key (ClaimId) references Claim(Id)
+)
+
 create table UserType
 (
 	Id int identity primary key,
@@ -32,10 +53,18 @@ create table [User]
 	Password nvarchar(20),
 	IdentityNo nvarchar(11),
 	Phone nvarchar(11),
-	IsDeleted bit,
 	TownId int,
 	UserTypeId int,
 	Constraint FK_UserId_TownId foreign key (TownId) references Town(Id),
 	Constraint FK_UserId_UserTypeId foreign key (UserTypeId) references UserType(Id)
 )
-///////// salih ençevik son ////////////
+go
+create table UserRole
+(
+	Id int identity primary key,
+	UserId int,
+	RoleId int
+	Constraint FK_UserRoleId_RoleId foreign key (RoleId) references Role(Id),
+	Constraint FK_UserRoleId_UserId foreign key (UserId) references [User](Id)
+)
+----////////////////// salih ençevik son ////////////
