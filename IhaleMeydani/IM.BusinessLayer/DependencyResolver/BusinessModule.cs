7c6ee2b64887dce6844902a321a3a6729058001f
Muhammed Göksel Kalyon <muhammedgokselkalyon@stu.ayvansaray.edu.tr>
2020-04-09 04:20:34 +0300

    Ninject IoC container oluşturuldu ve Automapping entegre edildi

---
+++ IhaleMeydani/IM.BusinessLayer/DependencyResolver/BusinessModule.cs
@@ -0,0 +1,25 @@
+﻿using IM.BusinessLayer.Abstract;
+using IM.BusinessLayer.Concrete;
+using IM.BusinessLayer.ServiceAdapter;
+using IM.DataAccessLayer.Abstract;
+using IM.DataAccessLayer.Concrete.EFConcrete;
+using IM.DataLayer;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessLayer.DependencyResolver
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDataBusinessService<log>>().To<LogManager>().InSingletonScope();
+            Bind<IDataAccessDal<log>>().To<LogConcrete>().InSingletonScope();
+            Bind<ITCService>().To<TCServiceAdapter>().InSingletonScope();
+        }
+    }
+}
