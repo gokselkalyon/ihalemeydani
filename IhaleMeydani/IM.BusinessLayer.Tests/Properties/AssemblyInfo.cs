bf2a69b29e75eeea4d057b1876d1b56d0aa0e777
salıh encevık <salihencevik@gmail.com>
2020-04-10 18:40:49 +0300

    Ara commit

---
+++ IhaleMeydani/IM.BusinessLayer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IM.BusinessLayer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IM.BusinessLayer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b58021d-4b8e-4207-8d02-a6bdefd54b0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IhaleMeydani/IM.BusinessLayer/Mapping/AutomMapper/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using IM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessLayer.Mapping.AutomMapper
+{
+    public class BusinessProfile:Profile
+    {
+        public BusinessProfile()
+        {
+            CreateMap<log, log>();
+        }
+    }
+}
