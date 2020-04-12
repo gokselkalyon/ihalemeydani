0c33ac3ccaf12c01e2538d6a587eb6c6f5fd0112
Furkan Özbay <furkanozb37@hotmail.com>
2020-04-13 01:14:43 +0300

    WCF Service V1

---
+++ IhaleMeydani/IM.ServiceLayer/IIhaleService.cs
@@ -0,0 +1,186 @@
+﻿using IM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IM.ServiceLayer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIhaleService" in both code and config file together.
+    [ServiceContract]
+    public interface IIhaleService
+    {
+        #region log
+        [OperationContract]
+        List<log> GetLogs();
+
+        [OperationContract]
+        log GetLog(int Id);
+
+        [OperationContract]
+        void AddLog(log entity);
+
+        [OperationContract]
+        void RemoveLog(int Id);
+
+        [OperationContract]
+        void UpdateLog(log entity);
+        #endregion
+
+        #region AMOUNT_OF_INCREASE
+        [OperationContract]
+        List<AMOUNT_OF_INCREASE> GetsAmountofIncrease();
+
+        [OperationContract]
+        AMOUNT_OF_INCREASE GetAmountofIncrease(int Id);
+
+        [OperationContract]
+        void AddAmountofIncrease(AMOUNT_OF_INCREASE entity);
+
+        [OperationContract]
+        void RemoveAmountofIncrease(int Id);
+
+        [OperationContract]
+        void UpdateAmountofIncrease(AMOUNT_OF_INCREASE entity);
+        #endregion
+
+        #region auction
+        [OperationContract]
+        List<auction> GetAuctions();
+
+        [OperationContract]
+        auction GetAuction(int Id);
+
+        [OperationContract]
+        void AddAuction(auction entity);
+
+        [OperationContract]
+        void RemoveAuction(int Id);
+
+        [OperationContract]
+        void UpdateAuction(auction entity);
+        #endregion
+
+        #region bank
+        [OperationContract]
+        List<bank> GetBanks();
+
+        [OperationContract]
+        bank GetBank(int Id);
+
+        [OperationContract]
+        void AddBank(bank entity);
+
+        [OperationContract]
+        void RemoveBank(int Id);
+
+        [OperationContract]
+        void UpdateBank(bank entity);
+        #endregion
+
+        #region CarBrand
+        [OperationContract]
+        List<CarBrand> GetCarBrands();
+
+        [OperationContract]
+        CarBrand GetCarBrand(int Id);
+
+        [OperationContract]
+        void AddCarBrand(CarBrand entity);
+
+        [OperationContract]
+        void RemoveCarBrand(int Id);
+
+        [OperationContract]
+        void UpdateCarBrand(CarBrand entity);
+        #endregion
+
+        #region CarDetail
+        [OperationContract]
+        List<CarDetail> GetCarDetails();
+
+        [OperationContract]
+        CarDetail GetCarDetail(int Id);
+
+        [OperationContract]
+        void AddCarDetail(CarDetail entity);
+
+        [OperationContract]
+        void RemoveCarDetail(int Id);
+
+        [OperationContract]
+        void UpdateCarDetail(CarDetail entity);
+        #endregion
+
+        #region CarHardwareDetail
+        [OperationContract]
+        List<CarHardwareDetail> GetCarHardwareDetails();
+
+        [OperationContract]
+        CarHardwareDetail GetCarHardwareDetail(int Id);
+
+        [OperationContract]
+        void AddCarHardwareDetail(CarHardwareDetail entity);
+
+        [OperationContract]
+        void RemoveCarHardwareDetail(int Id);
+
+        [OperationContract]
+        void UpdateCarHardwareDetail(CarHardwareDetail entity);
+        #endregion
+
+        #region CarMake
+        [OperationContract]
+        List<CarMake> GetCarMakes();
+
+        [OperationContract]
+        CarMake GetCarMake(int Id);
+
+        [OperationContract]
+        void AddCarMake(CarMake entity);
+
+        [OperationContract]
+        void RemoveCarMake(int Id);
+
+        [OperationContract]
+        void UpdateCarMake(CarMake entity);
+        #endregion
+
+        #region CarTechnicalDetail
+        [OperationContract]
+        List<CarTechnicalDetail> GetCarTechnicalDetails();
+
+        [OperationContract]
+        CarTechnicalDetail GetCarTechnicalDetail(int Id);
+
+        [OperationContract]
+        void AddCarTechnicalDetail(CarTechnicalDetail entity);
+
+        [OperationContract]
+        void RemoveCarTechnicalDetail(int Id);
+
+        [OperationContract]
+        void UpdateCarTechnicalDetail(CarTechnicalDetail entity);
+        #endregion
+
+        #region city
+        [OperationContract]
+        List<city> GetCities();
+
+        [OperationContract]
+        city GetCity(int Id);
+
+        [OperationContract]
+        void AddCity(city entity);
+
+        [OperationContract]
+        void RemoveCity(int Id);
+
+        [OperationContract]
+        void UpdateCity(city entity);
+        #endregion
+
+    }
+}
---
+++ IhaleMeydani/IM.ServiceLayer/IhaleService.svc.cs
@@ -0,0 +1,444 @@
+﻿using IM.BusinessLayer.Abstract;
+using IM.BusinessLayer.DependencyResolver;
+using IM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IM.ServiceLayer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "IhaleService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select IhaleService.svc or IhaleService.svc.cs at the Solution Explorer and start debugging.
+    public class IhaleService : IIhaleService
+    {
+        #region log
+        public void AddLog(log entity)
+        {
+            using (IDataBusinessService<log> _db = InstanceFactory.GetInstance<IDataBusinessService<log>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public log GetLog(int Id)
+        {
+            using (IDataBusinessService<log> _db = InstanceFactory.GetInstance<IDataBusinessService<log>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public List<log> GetLogs()
+        {
+            using (IDataBusinessService<log> _db = InstanceFactory.GetInstance<IDataBusinessService<log>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public void RemoveLog(int Id)
+        {
+            using (IDataBusinessService<log> _db = InstanceFactory.GetInstance<IDataBusinessService<log>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateLog(log entity)
+        {
+            using (IDataBusinessService<log> _db = InstanceFactory.GetInstance<IDataBusinessService<log>>())
+            {
+                _db.Update(entity);
+            }
+        }
+        #endregion
+
+        #region AMOUNT_OF_INCREASE
+        public void UpdateAmountofIncrease(AMOUNT_OF_INCREASE entity)
+        {
+            using (IDataBusinessService<AMOUNT_OF_INCREASE> _db = InstanceFactory.GetInstance<IDataBusinessService<AMOUNT_OF_INCREASE>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        public AMOUNT_OF_INCREASE GetAmountofIncrease(int Id)
+        {
+            using (IDataBusinessService<AMOUNT_OF_INCREASE> _db = InstanceFactory.GetInstance<IDataBusinessService<AMOUNT_OF_INCREASE>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddAmountofIncrease(AMOUNT_OF_INCREASE entity)
+        {
+            using (IDataBusinessService<AMOUNT_OF_INCREASE> _db = InstanceFactory.GetInstance<IDataBusinessService<AMOUNT_OF_INCREASE>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public List<AMOUNT_OF_INCREASE> GetsAmountofIncrease()
+        {
+            using (IDataBusinessService<AMOUNT_OF_INCREASE> _db = InstanceFactory.GetInstance<IDataBusinessService<AMOUNT_OF_INCREASE>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public void RemoveAmountofIncrease(int Id)
+        {
+            using (IDataBusinessService<AMOUNT_OF_INCREASE> _db = InstanceFactory.GetInstance<IDataBusinessService<AMOUNT_OF_INCREASE>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+        #endregion
+
+        #region auction
+        public List<auction> GetAuctions()
+        {
+            using (IDataBusinessService<auction> _db = InstanceFactory.GetInstance<IDataBusinessService<auction>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        auction IIhaleService.GetAuction(int Id)
+        {
+            using (IDataBusinessService<auction> _db = InstanceFactory.GetInstance<IDataBusinessService<auction>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddAuction(auction entity)
+        {
+            using (IDataBusinessService<auction> _db = InstanceFactory.GetInstance<IDataBusinessService<auction>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void UpdateAuction(auction entity)
+        {
+            using (IDataBusinessService<auction> _db = InstanceFactory.GetInstance<IDataBusinessService<auction>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        public void RemoveAuction(int Id)
+        {
+            using (IDataBusinessService<auction> _db = InstanceFactory.GetInstance<IDataBusinessService<auction>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+        #endregion
+
+        #region bank
+
+        public List<bank> GetBanks()
+        {
+            using (IDataBusinessService<bank> _db = InstanceFactory.GetInstance<IDataBusinessService<bank>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public bank GetBank(int Id)
+        {
+            using (IDataBusinessService<bank> _db = InstanceFactory.GetInstance<IDataBusinessService<bank>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddBank(bank entity)
+        {
+            using (IDataBusinessService<bank> _db = InstanceFactory.GetInstance<IDataBusinessService<bank>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveBank(int Id)
+        {
+            using (IDataBusinessService<bank> _db = InstanceFactory.GetInstance<IDataBusinessService<bank>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateBank(bank entity)
+        {
+            using (IDataBusinessService<bank> _db = InstanceFactory.GetInstance<IDataBusinessService<bank>>())
+            {
+                _db.Update(entity);
+            }
+        }
+        #endregion
+
+        #region CarBrand
+
+        public List<CarBrand> GetCarBrands()
+        {
+            using (IDataBusinessService<CarBrand> _db = InstanceFactory.GetInstance<IDataBusinessService<CarBrand>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public CarBrand GetCarBrand(int Id)
+        {
+            using (IDataBusinessService<CarBrand> _db = InstanceFactory.GetInstance<IDataBusinessService<CarBrand>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCarBrand(CarBrand entity)
+        {
+            using (IDataBusinessService<CarBrand> _db = InstanceFactory.GetInstance<IDataBusinessService<CarBrand>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCarBrand(int Id)
+        {
+            using (IDataBusinessService<CarBrand> _db = InstanceFactory.GetInstance<IDataBusinessService<CarBrand>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateCarBrand(CarBrand entity)
+        {
+            using (IDataBusinessService<CarBrand> _db = InstanceFactory.GetInstance<IDataBusinessService<CarBrand>>())
+            {
+                _db.Update(entity);
+            }
+        }
+        #endregion
+
+        #region CarDetail
+
+        public List<CarDetail> GetCarDetails()
+        {
+            using (IDataBusinessService<CarDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarDetail>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public CarDetail GetCarDetail(int Id)
+        {
+            using (IDataBusinessService<CarDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarDetail>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCarDetail(CarDetail entity)
+        {
+            using (IDataBusinessService<CarDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarDetail>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCarDetail(int Id)
+        {
+            using (IDataBusinessService<CarDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarDetail>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateCarDetail(CarDetail entity)
+        {
+            using (IDataBusinessService<CarDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarDetail>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        #endregion
+
+        #region CarHardwareDetail
+        public List<CarHardwareDetail> GetCarHardwareDetails()
+        {
+            using (IDataBusinessService<CarHardwareDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarHardwareDetail>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public CarHardwareDetail GetCarHardwareDetail(int Id)
+        {
+            using (IDataBusinessService<CarHardwareDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarHardwareDetail>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCarHardwareDetail(CarHardwareDetail entity)
+        {
+            using (IDataBusinessService<CarHardwareDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarHardwareDetail>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCarHardwareDetail(int Id)
+        {
+            using (IDataBusinessService<CarHardwareDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarHardwareDetail>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateCarHardwareDetail(CarHardwareDetail entity)
+        {
+            using (IDataBusinessService<CarHardwareDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarHardwareDetail>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        #endregion
+
+        #region CarMake
+        public List<CarMake> GetCarMakes()
+        {
+            using (IDataBusinessService<CarMake> _db = InstanceFactory.GetInstance<IDataBusinessService<CarMake>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public CarMake GetCarMake(int Id)
+        {
+            using (IDataBusinessService<CarMake> _db = InstanceFactory.GetInstance<IDataBusinessService<CarMake>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCarMake(CarMake entity)
+        {
+            using (IDataBusinessService<CarMake> _db = InstanceFactory.GetInstance<IDataBusinessService<CarMake>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCarMake(int Id)
+        {
+            using (IDataBusinessService<CarMake> _db = InstanceFactory.GetInstance<IDataBusinessService<CarMake>>())
+
+                _db.Remove(Id);
+        }
+        public void UpdateCarMake(CarMake entity)
+        {
+            using (IDataBusinessService<CarMake> _db = InstanceFactory.GetInstance<IDataBusinessService<CarMake>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        #endregion
+
+        #region CarTechnicalDetail
+        public List<CarTechnicalDetail> GetCarTechnicalDetails()
+        {
+            using (IDataBusinessService<CarTechnicalDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarTechnicalDetail>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public CarTechnicalDetail GetCarTechnicalDetail(int Id)
+        {
+            using (IDataBusinessService<CarTechnicalDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarTechnicalDetail>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCarTechnicalDetail(CarTechnicalDetail entity)
+        {
+            using (IDataBusinessService<CarTechnicalDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarTechnicalDetail>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCarTechnicalDetail(int Id)
+        {
+            using (IDataBusinessService<CarTechnicalDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarTechnicalDetail>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateCarTechnicalDetail(CarTechnicalDetail entity)
+        {
+            using (IDataBusinessService<CarTechnicalDetail> _db = InstanceFactory.GetInstance<IDataBusinessService<CarTechnicalDetail>>())
+            {
+                _db.Update(entity);
+            }
+        }
+
+        #endregion
+
+        #region City
+
+        public List<city> GetCities()
+        {
+            using (IDataBusinessService<city> _db = InstanceFactory.GetInstance<IDataBusinessService<city>>())
+            {
+                return _db.GetAll();
+            }
+        }
+
+        public city GetCity(int Id)
+        {
+            using (IDataBusinessService<city> _db = InstanceFactory.GetInstance<IDataBusinessService<city>>())
+            {
+                return _db.Get(Id);
+            }
+        }
+
+        public void AddCity(city entity)
+        {
+            using (IDataBusinessService<city> _db = InstanceFactory.GetInstance<IDataBusinessService<city>>())
+            {
+                _db.Add(entity);
+            }
+        }
+
+        public void RemoveCity(int Id)
+        {
+            using (IDataBusinessService<city> _db = InstanceFactory.GetInstance<IDataBusinessService<city>>())
+            {
+                _db.Remove(Id);
+            }
+        }
+
+        public void UpdateCity(city entity)
+        {
+            using (IDataBusinessService<city> _db = InstanceFactory.GetInstance<IDataBusinessService<city>>())
+            {
+                _db.Update(entity);
+            }
+        }
+        #endregion
+    }
+
+}
