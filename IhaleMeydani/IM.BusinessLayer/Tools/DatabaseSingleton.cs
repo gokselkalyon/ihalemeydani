7f4e6409d07db08c67ae714e02506023ef27993f
Mesut Solak <mesuttsolak@gmail.com>
2020-04-25 12:30:33 +0300

    Veritabanı için singleton design pattern'ın eklenmesi

---
+++ IhaleMeydani/IM.BusinessLayer/Tools/DatabaseSingleton.cs
@@ -0,0 +1,16 @@
+﻿using IM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessLayer.Tools
+{
+    public class DatabaseSingleton
+    {
+        public readonly static IHALEDBEntities _db;
+
+        public static IHALEDBEntities db => _db ?? new IHALEDBEntities();
+    }
+}
