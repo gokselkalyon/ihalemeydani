d335965ea5e0f1ce793bd3ba7f52e5f9e8c54343
Furkan Özbay <furkanozb37@hotmail.com>
2020-04-29 12:35:04 +0300

    Quick Sell Controllerı oluşturuldu.

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/QuickSellController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{
+    public class QuickSellController : Controller
+    {
+        // GET: QuickSell
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
