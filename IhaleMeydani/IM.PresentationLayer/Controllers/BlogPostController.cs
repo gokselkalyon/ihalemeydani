b47a7f01f5e11abb7fc0ad7acc2cb664f6431a49
Taha <tahatopac@hotmail.com>
2020-05-02 17:47:00 +0300

    Login page eklendi

--- IhaleMeydani/IM.PresentationLayer/Controllers/BlogPostController.cs
+++ IhaleMeydani/IM.PresentationLayer/Controllers/BlogPostController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IM.PresentationLayer.IhaleWCFService;
+using IM.PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +9,14 @@ using System.Web.Mvc;
 namespace IM.PresentationLayer.Controllers
 {
     public class BlogPostController : Controller
-    {    
+    {
+        IhaleServiceClient Db = new IhaleServiceClient();
+        BlogModelView bm = new BlogModelView();
         [Route("Blog")]
         public ActionResult Index()
         {
-            return View();
+            bm.Posts = Db.GetPosts().ToList();
+            return View(bm);
         }
         [Route("BlogContent")]
         public ActionResult BlogIndex()
---
+++ IhaleMeydani/IM.PresentationLayer/Models/BlogModelView.cs
@@ -0,0 +1,15 @@
+﻿using IM.PresentationLayer.IhaleWCFService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IM.PresentationLayer.Models
+{
+    public class BlogModelView
+    {
+        public List<tag> Tags { get; set; }
+        public List<tag_post> Tags_posts { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+}
