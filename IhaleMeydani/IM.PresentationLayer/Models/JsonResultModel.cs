89951ccd6ef5217cb908822e8561fe44ab8378e0
Mesut Solak <mesuttsolak@gmail.com>
2020-05-06 20:45:33 +0300

    JsonResultModel'in oluşturulması

---
+++ IhaleMeydani/IM.PresentationLayer/Models/JsonResultModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IM.PresentationLayer.Models
+{
+    public class JsonResultModel
+    {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Modal { get; set; }
+        public string Function { get; set; }
+    }
+}
