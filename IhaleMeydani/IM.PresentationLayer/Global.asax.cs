8fd7d284197d8626dc33d2c952acfa1aa521d458
Mesut Solak <mesuttsolak@gmail.com>
2020-05-07 01:13:16 +0300

    Global asaxta hataların yakalanması

--- IhaleMeydani/IM.PresentationLayer/Global.asax.cs
+++ IhaleMeydani/IM.PresentationLayer/Global.asax.cs
@@ -16,7 +16,46 @@ namespace IM.PresentationLayer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
 
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError(); //Oluşan hatayı değişkene atadık.
+                                                   //Eğer hata kaydı (log) tutulacak ise gerekli kodlar buraya.
+            var httpException = exception as HttpException;
+            Response.Clear();
+            Server.ClearError(); //Sunucudaki hatayı temizledik.
+            Response.TrySkipIisCustomErrors = true; //IIS'in tipik hata sayfalarını görmezden geldik.
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error"; //Hata mesajlarını yöneteceğimiz Controller ismi
+            routeData.Values["action"] = "PageError"; //Controller içindeki default Action ismi
+            routeData.Values["exception"] = exception;
+            //Response.StatusCode = 500;
+
+            if (httpException != null)
+            {
+                Response.StatusCode = httpException.GetHttpCode();
+
+                switch (Response.StatusCode)
+                {
+                    case 403: //Eğer 403 hatası meydana gelmiş ise Http403 Action'ı devreye girecek.
+                        routeData.Values["action"] = "Page403";
+                        break;
+                    case 404: //Eğer 404 hatası meydana gelmiş ise Http404 Action'ı devreye girecek.
+                        routeData.Values["action"] = "Page404";
+                        break;
+                    case 401:
+                        routeData.Values["Action"] = "Page401";
+                        break;
+                    case 500:
+                        routeData.Values["Action"] = "Page500";
+                        break;
+                }
+            }
+
+            IController errorsController = new Controllers.ErrorsController();
+            var rc = new RequestContext(new HttpContextWrapper(Context), routeData);
+            errorsController.Execute(rc);
         }
     }
 }
