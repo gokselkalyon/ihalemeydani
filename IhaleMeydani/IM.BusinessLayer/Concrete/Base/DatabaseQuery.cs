5fb89a82365f08f0bf79d1a2d7900602d1915dbf
Mesut Solak <mesuttsolak@gmail.com>
2020-05-06 20:25:32 +0300

    Database Query'nin Entegre Edilmesi

---
+++ IhaleMeydani/IM.BusinessLayer/Concrete/Base/DatabaseQuery.cs
@@ -0,0 +1,18 @@
+﻿using IM.BusinessLayer.Abstract;
+using IM.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IM.BusinessLayer.Concrete.Base
+{
+    public class DatabaseQuery : BaseModal, IDatabaseQuery
+    {
+        public List<TT> QueryList<TT>(string query)
+        {
+            return db.Database.SqlQuery<TT>(query).ToList();
+        }
+    }
+}
