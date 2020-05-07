aa50d37dcf8773985c74bd78e7d04671a4c1f603
Mesut Solak <mesuttsolak@gmail.com>
2020-05-07 23:07:42 +0300

    Dinamik Menü classlarının oluşturulması

--- IhaleMeydani/IM.DataAccessLayer/Concrete/EFConcrete/MenuConcrete.cs
+++ IhaleMeydani/IM.DataAccessLayer/Concrete/EFConcrete/MenuConcrete.cs
@@ -12,10 +12,17 @@ namespace IM.DataAccessLayer.Concrete.EFConcrete
 {
     public class MenuConcrete : BaseConcrete, IDataAccessDal<Menu>
     {
+        public int MainCount = 0;
+        StringBuilder sb = new StringBuilder();
         public void Add(Menu entity)
         {
             DB.Menus.Add(entity);
             DB.SaveChanges();
+        }
+
+        public List<Menu> SubMenu(int id)
+        {
+            return DB.Menus.Where(x => x.MenuId == id).ToList();
         }
 
         public Menu Get(int id)
@@ -50,6 +57,38 @@ namespace IM.DataAccessLayer.Concrete.EFConcrete
             DB.Menus.Attach(t);
             DB.Entry(t).State = System.Data.Entity.EntityState.Modified;
             DB.SaveChanges();
+        }
+        public StringBuilder MainMenu()
+        {
+            sb.Clear();
+            sb.Append("<ul class='nav navbar-nav navbar-left'>");
+
+            var _MainMenu = GetFilter(x => x.MenuId == 0);
+            foreach (var mainmenu in _MainMenu)
+            {
+                MainCount++;
+                sb.Append("<li class='has-dropdown'><a data-toggle='dropdown' class='dropdown-toggle' href='" + mainmenu.Name + "'>" + mainmenu.Name + "</a>");
+                SubCategory(mainmenu.Id);
+                sb.Append("</li>");
+            }
+            sb.Append("</ul>");
+            return sb;
+        }
+        private void SubCategory(int id)
+        {
+            int say = SubMenu(id).Count;
+            if (say > 0)
+            {
+                var _submenu = SubMenu(id);
+                sb.Append("<ul class='dropdown-menu'>");
+                foreach (Menu submenus in _submenu)
+                {
+                    sb.Append("<li><a href='" + submenus.Name + "'>" + submenus.Name + "</a>");
+                    SubCategory(submenus.Id);//Eger alt kategorinin de alt kategorisi var ise Altkategori metoduna gönderiyoruz
+                    sb.Append("</li>");
+                }
+                sb.Append("</ul>");
+            }
         }
     }
 }
