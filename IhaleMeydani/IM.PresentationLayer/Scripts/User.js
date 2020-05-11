ac36e841c1729c504a8f4dee8baa5aa50c6cee99
salıh encevık <salihencevik@gmail.com>
2020-05-12 00:16:35 +0300

    user Kullanıcı Engelleme Sorunları düzeltildi

--- IhaleMeydani/IM.PresentationLayer/Scripts/User.js
+++ IhaleMeydani/IM.PresentationLayer/Scripts/User.js
@@ -1,6 +1,5 @@
 ﻿$(document).ready(function () {
-    $(document).on("click", ".block-user", function () {
-        debugger;
+    $(document).on("click", ".block-user", function () { 
         var id = +$(this).attr("data-id");
         var deleted = $(this).attr("data-bind"); 
         if (deleted == "True") {
@@ -15,8 +14,7 @@
                 cancelButtonText: "Hayır, vazgeç!",
             },
                 function (isConfirm) {
-                    if (isConfirm) {
-                        debugger;
+                    if (isConfirm) { 
                         $.ajax({
                             url: '/User/UserBlock/',
                             data: { "id": id },
@@ -24,10 +22,17 @@
                             dataType: 'json',
                             success: function (result) {
                                 if (result == 1) {
+                                    debugger;
                                     $("#" + id).empty();
                                     $("#" + id).append(
                                         'Engelle'
-                                    )
+                                    );
+                                    $("." + id).empty();
+                                    $("." + id).append(
+                                        'Hayır'
+                                    );
+                                    $("#" + id).removeAttr("data-bind");
+                                    $("#" + id).attr("data-bind", "False");
                                 }
                                 else {
                                     swal("Başarısız!", "Engel Kaldırma İşlemi Gerçekleşmedi!");
@@ -60,7 +65,13 @@
                                     $("#" + id).empty();
                                     $("#" + id).append(
                                         'Engeli Kaldır'
-                                    )
+                                    );
+                                    $("." + id).empty();
+                                    $("." + id).append(
+                                        'Evet'
+                                    );
+                                    $("#" + id).removeAttr("data-bind");
+                                    $("#" + id).attr("data-bind", "True");
                                 }
                                 else {
                                     swal("Başarısız!", "Engelleme İşlemi Gerçekleşmedi!");
