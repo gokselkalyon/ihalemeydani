8c13481f30c494343f033da72a729299aa4ad5a4
salıh encevık <salihencevik@gmail.com>
2020-05-11 23:41:59 +0300

    User Block

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using IM.PresentationLayer.IhaleWCFService;
+using IM.PresentationLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{ 
+    public class UserController : Controller
+    {
+        UserModelView user = new UserModelView();
+        IhaleServiceClient ihaleClient = new IhaleServiceClient();
+        // GET: User
+        [HttpGet] 
+        [Route("User")]
+        public ActionResult Index()
+        {
+            var query = (from r in ihaleClient.GetUsers()
+                         select new UserModel
+                         {
+                             Id = r.Id,
+                             Name = r.Name,
+                             Surname = r.Surname,
+                             Email = r.Email,
+                             Adress = r.Adress, 
+                             CompanyName = r.CompanyName,
+                             DateOfBirt = r.Dateofbird,
+                             IdentityNo = r.IdentityNo,
+                             IsDeleted = r.IsDeleted,
+                             Username = r.UserName,
+                             Phone = r.Phone
+                         }).ToList();
+            user.userModel = query;
+            return View(user);
+        }
+        public JsonResult UserBlock(int id) 
+        {
+            try
+            {
+                var user = ihaleClient.GetUser(id);
+                if (user.IsDeleted == true)
+                    user.IsDeleted = false;
+                else
+                    user.IsDeleted = true;
+                ihaleClient.UpdateUser(user);
+                return Json(1, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            { 
+                return Json(0, JsonRequestBehavior.AllowGet);
+            }
+            
+
+        }
+    }
+}
---
+++ IhaleMeydani/IM.PresentationLayer/Scripts/User.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".block-user", function () {
+        debugger;
+        var id = +$(this).attr("data-id");
+        var deleted = $(this).attr("data-bind"); 
+        if (deleted == "True") {
+            swal({
+                title: "Emin misiniz?",
+                text: "<strong>Kullanıcı Engeli Kaldırılacak!</strong>",
+                type: "warning",
+                html: true,
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Evet, Kaldır!",
+                cancelButtonText: "Hayır, vazgeç!",
+            },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        debugger;
+                        $.ajax({
+                            url: '/User/UserBlock/',
+                            data: { "id": id },
+                            type: "POST",
+                            dataType: 'json',
+                            success: function (result) {
+                                if (result == 1) {
+                                    $("#" + id).empty();
+                                    $("#" + id).append(
+                                        'Engelle'
+                                    )
+                                }
+                                else {
+                                    swal("Başarısız!", "Engel Kaldırma İşlemi Gerçekleşmedi!");
+                                }
+                            }
+                        });
+                    }
+                });
+        } else {
+            swal({
+                title: "Emin misiniz?",
+                text: "<strong>Kullanıcı Engellenecek!</strong>",
+                type: "warning",
+                html: true,
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Evet, Engelle!",
+                cancelButtonText: "Hayır, vazgeç!",
+            },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        debugger;
+                        $.ajax({
+                            url: '/User/UserBlock/',
+                            data: { "id": id },
+                            type: "POST",
+                            dataType: 'json',
+                            success: function (result) {
+                                if (result == 1) {
+                                    $("#" + id).empty();
+                                    $("#" + id).append(
+                                        'Engeli Kaldır'
+                                    )
+                                }
+                                else {
+                                    swal("Başarısız!", "Engelleme İşlemi Gerçekleşmedi!");
+                                }
+                            }
+                        });
+                    }
+                });
+        }
+        
+    });
+});
