02b78752d34d0e0a32eb43aa005e54f56616c319
Muhammed Göksel Kalyon <muhammedgokselkalyon@stu.ayvansaray.edu.tr>
2020-05-12 07:03:46 +0300

    UserProductModel güncellendi , MultiAdded , MultiUpdate oluşturuldu entegre edildi, IDatabasequery güncellendi

--- IhaleMeydani/IM.DataAccessLayer/Abstract/IDatabaseQuery.cs
+++ IhaleMeydani/IM.DataAccessLayer/Abstract/IDatabaseQuery.cs
@@ -9,5 +9,9 @@ namespace IM.DataAccessLayer.Abstract
     public interface IDataBaseQuery<T> where T:class,new()
     {
         List<T> QueryList();
+        int MultiAdded(T t);
+        int Multiupdate(T t);
+        /* örnek userproductmodel içerisinde 5 farklı tablo barındırıyor
+         * bunları gerekli tablolara eklemek amaçlı kullanacağı*/
     }
 }
