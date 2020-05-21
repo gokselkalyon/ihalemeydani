b59b0cfb998a70b26e473b73ad2aa63ac5a6874d
Mesut Solak <mesuttsolak@gmail.com>
2020-05-21 16:28:26 +0300

    Admin tarafına menülerin eklenmesi

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{
+    //[RoutePrefix("Menü")]
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        [Route("Menüler")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
