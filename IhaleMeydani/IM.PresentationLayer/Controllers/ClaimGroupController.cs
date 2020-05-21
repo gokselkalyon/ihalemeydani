c002168cf858037b9b9f02e2914cee99672db21f
salıh encevık <salihencevik@gmail.com>
2020-05-21 21:52:17 +0300

    claim grup ekleme işlemi

--- IhaleMeydani/IM.PresentationLayer/Controllers/ClaimGroupController.cs
+++ IhaleMeydani/IM.PresentationLayer/Controllers/ClaimGroupController.cs
@@ -1,4 +1,5 @@
 ﻿using IM.PresentationLayer.IhaleWCFService;
+using IM.PresentationLayer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,24 @@ namespace IM.PresentationLayer.Controllers
 {
     public class ClaimGroupController : Controller
     {
+        ClaimGroupModelView cgmv = new ClaimGroupModelView();
         // GET: ClaimGroup
         IhaleServiceClient ihaleClient = new IhaleServiceClient();
         public ActionResult Index()
         {
             var claimGroup = ihaleClient.GetClaimGroups();
             return View(claimGroup);
+        }
+        public ActionResult AddClaimGroup()
+        { 
+            return View();
+        }
+        public ActionResult ClaimGroupAdd(ClaimGroupModelView cgmv)
+        {
+            ClaimGroup cg = new ClaimGroup();
+            cg.Name = cgmv.ClaimGroupName;
+            ihaleClient.AddClaimGroup(cg);
+            return RedirectToAction("index");
         }
     }
 }
