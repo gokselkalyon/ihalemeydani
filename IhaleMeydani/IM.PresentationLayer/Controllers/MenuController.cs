39601141c8d02ba1dcfb16c4543122206a533f30
Mesut Solak <mesuttsolak@gmail.com>
2020-05-21 23:08:20 +0300

    Admin Menü için genel yapının kurulması

--- IhaleMeydani/IM.PresentationLayer/Controllers/MenuController.cs
+++ IhaleMeydani/IM.PresentationLayer/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IM.PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,13 +8,34 @@ using System.Web.Mvc;
 namespace IM.PresentationLayer.Controllers
 {
     //[RoutePrefix("Menü")]
-    public class MenuController : Controller
+    public class MenuController : BaseController
     {
+        List<MenuModel> menus = new List<MenuModel>();
         // GET: Menu
         [Route("Menüler")]
         public ActionResult Index()
         {
             return View();
+        }
+        public PartialViewResult GetMenus()
+        {
+            menus.Clear();
+
+            var _menus = IhaleServiceClient.GetMenus().ToList();
+            foreach (var menu in _menus)
+            {
+                menus.Add(new MenuModel
+                {
+                    Id = menu.Id,
+                    Name = menu.Name,
+                    Description = menu.Description,
+                    Url = menu.Url,
+                    IconName = IhaleServiceClient.IconName(menu.IconId),
+                    SubMenu = IhaleServiceClient.GetMenu(menu.MenuId.Value).Name
+                });
+            }
+
+            return PartialView(menus);
         }
     }
 }
