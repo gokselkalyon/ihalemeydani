4e01fa7000d47474b73f58ab6b51c7df60dbd146
salıh encevık <salihencevik@gmail.com>
2020-05-21 21:42:02 +0300

    claim grup listeleme işleminin yapılması

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/ClaimGroupController.cs
@@ -0,0 +1,20 @@
+﻿using IM.PresentationLayer.IhaleWCFService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{
+    public class ClaimGroupController : Controller
+    {
+        // GET: ClaimGroup
+        IhaleServiceClient ihaleClient = new IhaleServiceClient();
+        public ActionResult Index()
+        {
+            var claimGroup = ihaleClient.GetClaimGroups();
+            return View(claimGroup);
+        }
+    }
+}
