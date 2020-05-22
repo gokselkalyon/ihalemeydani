dfa22b0fca1722a754087245a1e7287bbcb10220
Mesut Solak <mesuttsolak@gmail.com>
2020-05-23 00:05:07 +0300

    Menü Silme İşleminin Düzeltilmesi

--- IhaleMeydani/IM.PresentationLayer/Content/Template/js/menu.js
+++ IhaleMeydani/IM.PresentationLayer/Content/Template/js/menu.js
@@ -1,30 +1,38 @@
 ﻿$(document).on("click", ".MenuRemove", function () {
+    debugger;
     var _id = $(this).data("id");
     var _removeItem = $(this).parent().parent();
     var _menuname = $(_removeItem).children(":nth-child(1)").html();
 
-    Swal.fire({
-        title: 'Silme İşlemi',
-        text: _menuname+ " adlı menü silinsin mi ?",
-        icon: 'question',
+    swal({
+        title: "Emin misiniz?",
+        text: _menuname + "<strong> Adlı Menü  Silinecek!</strong>",
+        type: "warning",
+        html: true,
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Evet',
-        cancelButtonText: "Kapat"
-    }).then((result) => {
-        if (result.value) {
-            $.post("/RemoveMenu", { id: _id }, function (data) {
-                if (data.Description.includes("Başarıyla")) {
-                    SweetAlert(data.Icon, "Silme İşlemi", _menuname + " başarıyla silindi");
-                }
-                else {
-                    Message("error", "Bir hata oluştu");
-                }
-            });
-        }
-    });
- 
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Evet",
+        cancelButtonText: "Hayır",
+    },
+        function (isConfirm) {
+            if (isConfirm) {
+                $.post("/RemoveMenu", { id: _id }, function (data) {
+                    debugger;
+
+                    if (data.Description.includes("Başarıyla")) {
+                        _removeItem.remove();
+                        GeneralSweet("Başarılı", _menuname + "adlı menü başarıyla silindi", data.Icon);
+
+                    }
+                    else {
+                        GeneralSweet("Hata", "Bir hata Meydana Geldi", "Warning");
+                    }
+                });
+            }
+        });
 });
 
 
+function GeneralSweet(Title, Description, Icon) {
+    swal(Title, Description, Icon);
+}
