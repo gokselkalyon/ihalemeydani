d7309fcfc08124733031cd762cffe61b9d4c3481
Mesut Solak <mesuttsolak@gmail.com>
2020-05-24 14:50:13 +0300

    İkon Ekleme İşleminin Tamamlanması

---
+++ IhaleMeydani/IM.PresentationLayer/Content/Template/js/icon.js
@@ -0,0 +1,36 @@
+﻿function IconList() {
+    $.post("/IconList", null, function (result) {
+        $(".icon-list").html(result);
+    });
+}
+
+$("#btnIconAdd").on("click", function () {
+    $("#IconAddModal").modal("show");
+});
+
+$("#IconSave").on("click", function () {
+    $("#frmIconAdd").submit();
+});
+
+
+function GeneralSweet(Title, Description, Icon) {
+    swal(Title, Description, Icon);
+}
+
+
+function FormPost(FormId) {
+    $(document).find("#" + FormId).submit();
+}
+
+function FormClear(FormId) {
+    $(document).find("#" + FormId).trigger("reset");
+}
+
+function returnPostJson(data) {
+
+    if (data.Modal != null) {
+        $("#" + data.Modal).modal("hide");
+    }
+
+    GeneralSweet(data.Title, data.Description, data.Icon);
+}
--- IhaleMeydani/IM.PresentationLayer/Controllers/IconsController.cs
+++ IhaleMeydani/IM.PresentationLayer/Controllers/IconsController.cs
@@ -1,23 +1,44 @@
-﻿using System;
+﻿using IM.PresentationLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using I = IM.PresentationLayer.IhaleWCFService;
 
 namespace IM.PresentationLayer.Controllers
 {
     public class IconsController : BaseController
     {
-        // GET: Icons
         [Route("Icons")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("IconList")]
         public PartialViewResult List()
         {
             return PartialView(IhaleServiceClient.GetIcons().ToList());
+        }
+        public PartialViewResult Add()
+        {
+            return PartialView(new IconModelView());
+        }
+        [HttpPost]
+        public JsonResult IconAddOperation(IconModelView icon)
+        {
+            IhaleServiceClient.AddIcon(new I.Icon
+            {
+                Name = icon.Name
+            });
+
+            jsonResultModel.Title = "Ekleme İşlemi";
+            jsonResultModel.Icon = "success";
+            jsonResultModel.Modal = "IconAddModal";
+            jsonResultModel.Description = "İcon Başarıyla Eklendi";
+
+            return Json(jsonResultModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
