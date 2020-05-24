f9448d539f52a868af88e8cd5455b8bf781a15b5
Mesut Solak <mesuttsolak@gmail.com>
2020-05-24 16:31:05 +0300

    İletişimin Listelenmesinin Sağlanması

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{
+    public class ContactController : BaseController
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult ContactList()
+        {
+            return PartialView(IhaleServiceClient.GetContacts());
+        }
+
+    }
+}
