2bc2039a31fd7f1c90a6a403133d747c8783fd03
Mesut Solak <mesuttsolak@gmail.com>
2020-05-24 11:50:26 +0300

    İconlar Menüsünün Oluşturulması

---
+++ IhaleMeydani/IM.PresentationLayer/Controllers/IconsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IM.PresentationLayer.Controllers
+{
+    public class IconsController : Controller
+    {
+        // GET: Icons
+        [Route("Icons")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
